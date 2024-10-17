4ba9e1f79387efd7e6a6a1bf38eeb9c472f3251b
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-10-17 15:23:50 +0530

    added global variables

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+// global variables
+
+int board[3][3] = {
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'}};
+
+int current_player;
+
 int main(void)
 {
     cout << "\nHello World\n";
