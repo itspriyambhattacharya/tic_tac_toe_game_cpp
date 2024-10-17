a6d1f9466c0d7f11498a928a40aeeda4b12efa82
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-10-17 15:30:08 +0530

    added game board draw function and made a minor correction in board global variabe, changed the daatatype of the board array from int to char.

--- main.cpp
+++ main.cpp
@@ -3,15 +3,24 @@ using namespace std;
 
 // global variables
 
-int board[3][3] = {
+char board[3][3] = {
     {'1', '2', '3'},
     {'4', '5', '6'},
     {'7', '8', '9'}};
 
 int current_player;
 
+void drawBoard()
+{
+    cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
+    cout << "---|---|---" << endl;
+    cout << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
+    cout << "---|---|---" << endl;
+    cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
+}
+
 int main(void)
 {
-    cout << "\nHello World\n";
+    drawBoard();
     return 0;
 }
